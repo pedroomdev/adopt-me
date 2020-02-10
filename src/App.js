6a8c360c0900032e6dd80d02cba9b65f46a787e3
Pedro Ortega <pedro5022@hotmail.com>
2020-02-10 01:04:06 -0600

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,28 @@
+// your code is going to go here
+
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h3", {}, breed)
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese"
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Coktail"
+    }),
+    React.createElement(Pet, { name: "Doink", animal: "test", breed: "Donkey" })
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
