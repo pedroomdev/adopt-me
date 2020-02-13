f37d0dc0a6c3c744bb2e7e15930187daabd8504e
Pedro Ortega <pedro5022@hotmail.com>
2020-02-13 02:40:04 -0600

    Add hooks - Add react hooks plugin for eslint - Add custom generic useDropdown hook

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
-import { Pet } from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese"
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Coktail"
-    }),
-    React.createElement(Pet, { name: "Doink", animal: "test", breed: "Donkey" })
-  ]);
+  return (
+    <div>
+      <h1 id="something-important">Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  );
 };
 
 render(React.createElement(App), document.getElementById("root"));
